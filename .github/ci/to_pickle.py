07f551e2388db9d98c94a84cdc8c8cf9d30f0b34
y-i <y-i@users.noreply.github.com>
2025-12-04 20:49:54 +0900

    Add Public Suffix List update

--- .github/ci/to_pickle.py
+++ .github/ci/to_pickle.py
@@ -8,5 +8,5 @@ from src.checker_trie import ETLDChecker
 
 checker = ETLDChecker()
 checker.initialize_from_file('.github/ci/list.dat')
-with open("files/list.pickle", "wb") as f:
+with open('files/list.pickle', 'wb') as f:
     pickle.dump(checker, f)
